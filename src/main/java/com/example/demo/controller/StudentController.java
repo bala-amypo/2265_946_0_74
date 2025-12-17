4e3b06a55ec6fdc016205976d220dd8616315683
bala-amypo <bala@amypo.in>
2025-12-17 08:10:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -12,18 +12,18 @@ import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 
 @RestController
-public class StudentController {
+public class StudentController{
 
     @Autowired
     StudentService ser;
 
     @PostMapping("/adddata")
-    public Student createData(@RequestBody Student stu) {
+    public Student createData(@RequestBody Student stu){
         return ser.createData(stu);
     }
 
     @GetMapping("/fetchrecord")
-    public List<Student> fetchRecord() {
+    public List<Student>fetchRecord(){
         return
     }
 }
